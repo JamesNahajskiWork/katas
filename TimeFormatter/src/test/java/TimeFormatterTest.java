d67a78050f4a799eb5ad9f45205ba0485ae1e81a
James Nahajski and Krzysztof Makowski <pair+james.nahajski+krzysztof.makowski@sky.uk>
2023-01-24 14:56:01 +0000

    adding minesweeper frontend

--- TimeFormatter/src/test/java/TimeFormatterTest.java
+++ TimeFormatter/src/test/java/TimeFormatterTest.java
@@ -2,6 +2,7 @@ import org.example.TimeFormatter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TimeFormatterTest {
     @Test
@@ -12,5 +13,10 @@ public class TimeFormatterTest {
         assertEquals("2 minutes", TimeFormatter.formatDuration(120));
         assertEquals("1 hour", TimeFormatter.formatDuration(3600));
         assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
+    }
+
+    @Test
+    public void test() {
+        assertTrue("A".matches("[A-Z][A-Z]"));
     }
 }
